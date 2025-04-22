1814ac50278ec5c99b0d0fa9e8ec3dd89afa5919
Duriel7 <duriel@live.fr>
2025-04-22 13:57:16 +0200

    feat: added catabase handler and first migration

---
+++ Data/HeroContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Microsoft.EntityFrameworkCore;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using Heroes;
+
+public class HeroContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseSqlServer("Data Source = Duriel-Laptop; Initial Catalog = DurielHeroesClass; Integrated Security = True; Trust Server Certificate = True");
+    }
+    public HeroContext(DbContextOptions<HeroContext> options) : base(options)
+    {    }
+
+    public DbSet<Hero> Heroes { get; set; }
+}
--- Models/Hero.cs
+++ Models/Hero.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using Heroes;
 
 public class Hero
 {
+	public int Id { get; set; }
+	public string Name { get; set; }
+	public string Description { get; set; }
 	public Hero()
 	{
+
 	}
 }
